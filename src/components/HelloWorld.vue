4da3119b1ada5beef1652716bdcae676b4b1b7be
冼宜李 <xianyili@laifen.net>
2026-03-30 14:31:48 +0800

    feat: 第三次修

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">111---{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
